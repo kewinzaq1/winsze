adc77d6171eec8032e0c879e6f94da462ef1e45e
Szlezingier <kewinszlezingier@gmail.com>
2022-04-06 20:25:42 +0200

    ADD: fetchPost()

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,31 +1,58 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { addDoc, collection } from "firebase/firestore";
-import { db, storage } from "../../Auth";
-import { FeedHeading } from "./FeedHeading";
+import {getDownloadURL, ref, uploadBytes} from 'firebase/storage'
+import {addDoc, collection, query, getDocs, orderBy} from 'firebase/firestore'
+import {db, storage} from '../../Auth'
+import {FeedHeading} from './FeedHeading'
+import {v4 as uuidv4} from 'uuid'
+import React, {lazy, Suspense} from 'react'
+const Posts = lazy(() => import('./Posts'))
 
 export const Feed = () => {
-  return <FeedHeading />;
-};
+  return (
+    <>
+      <FeedHeading />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Posts />
+      </Suspense>
+    </>
+  )
+}
 
 export const uploadPost = async (user, desc, photo) => {
   if (photo) {
-    const imageRef = ref(storage, `PostsPhotos/`);
-    uploadBytes(imageRef, photo).then(() => {
-      getDownloadURL(imageRef).then(async (photoURL) => {
-        await addDoc(collection(db, "posts"), {
-          publisherNickname: user.displayName,
-          publisherPhotoURL: user.photoURL,
-          publishedDate: `${new Date().toLocaleTimeString("en")}`,
+    const imageRef = ref(storage, `PostsPhotos/${uuidv4()}`)
+    return uploadBytes(imageRef, photo).then(() => {
+      getDownloadURL(imageRef).then(async photoURL => {
+        await addDoc(collection(db, 'posts'), {
+          author: user.displayName,
+          avatar: user.photoURL,
+          date: `${new Date().toISOString()}`,
           description: desc,
-          photoURL: photoURL,
-        });
-      });
-    });
-  }
-  return await addDoc(collection(db, "posts"), {
-    publisherNickname: user.displayName,
-    publisherPhotoURL: user.photoURL,
-    publishedDate: `${new Date().toLocaleTimeString("en")}`,
-    description: desc,
-  });
-};
+          photo: photoURL,
+          id: Date.now(),
+        })
+      })
+    })
+  } else
+    return await addDoc(collection(db, 'posts'), {
+      author: user.displayName,
+      avatar: user.photoURL,
+      date: `${new Date().toISOString()}`,
+      description: desc,
+      id: uuidv4(),
+    })
+}
+
+export const fetchPosts = async () => {
+  let res = []
+  const postsRef = collection(db, 'posts')
+  const q = await query(postsRef, orderBy('date', 'asc'))
+
+  const querySnapshot = await getDocs(q)
+
+  querySnapshot.forEach(doc => {
+    console.log(doc)
+    res.push(doc.data())
+  })
+
+  return res
+}
