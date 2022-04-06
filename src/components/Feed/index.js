bdf3f901208fe4ea299decb2a57389877b9965e4
Szlezingier <kewinszlezingier@gmail.com>
2022-04-06 17:15:45 +0200

    ADD: split uploadPost() for two scenarios (with photo/without photo)

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,5 +1,31 @@
-import {FeedHeading} from './FeedHeading'
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { addDoc, collection } from "firebase/firestore";
+import { db, storage } from "../../Auth";
+import { FeedHeading } from "./FeedHeading";
 
 export const Feed = () => {
-  return <FeedHeading />
-}
+  return <FeedHeading />;
+};
+
+export const uploadPost = async (user, desc, photo) => {
+  if (photo) {
+    const imageRef = ref(storage, `PostsPhotos/`);
+    uploadBytes(imageRef, photo).then(() => {
+      getDownloadURL(imageRef).then(async (photoURL) => {
+        await addDoc(collection(db, "posts"), {
+          publisherNickname: user.displayName,
+          publisherPhotoURL: user.photoURL,
+          publishedDate: `${new Date().toLocaleTimeString("en")}`,
+          description: desc,
+          photoURL: photoURL,
+        });
+      });
+    });
+  }
+  return await addDoc(collection(db, "posts"), {
+    publisherNickname: user.displayName,
+    publisherPhotoURL: user.photoURL,
+    publishedDate: `${new Date().toLocaleTimeString("en")}`,
+    description: desc,
+  });
+};
