80f2e83dc58f838bc953abd9f04712332a6bf33f
Kewin Szlezingier <77052270+kewinzaq1@users.noreply.github.com>
2022-04-08 10:22:26 +0200

    FIX:  post apperance

--- src/components/Feed/Post.js
+++ src/components/Feed/Post.js
@@ -1,42 +1,82 @@
 /** @jsxImportSource @emotion/react */
 // eslint-disable-next-line no-unused-vars
 import {jsx, css} from '@emotion/react'
-import {Box, Paper, Typography} from '@mui/material'
+import {Avatar, Box, Paper, Typography} from '@mui/material'
 import Moment from 'react-moment'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 import {myBlue} from '../layout'
 
-export const Post = ({author, description, date, photo} = {}) => {
+const Post = ({author, description, date, photo, avatar} = {}) => {
   return (
     <Paper>
       <Box
         css={css`
-          padding: 1rem;
+          padding: 2rem;
+          gap: 1.25rem;
+          display: flex;
+          flex-direction: column;
         `}
       >
-        <Typography
-          variant="subtitle1"
-          component="p"
+        <Box
           css={css`
-            span {
-              background-color: ${myBlue};
-              color: #fff;
-              border-radius: 0.5rem;
-              padding: 0.5rem 1rem;
-            }
             display: flex;
-            align-items: center;
             justify-content: space-between;
-            gap: 1rem;
           `}
         >
-          <Moment fromNow>{date}</Moment>
-          <span>{author}</span>
-        </Typography>
-        <Typography variant="h5" component="h1">
+          <Box
+            css={css`
+              display: flex;
+              align-items: center;
+              gap: 0.5rem;
+            `}
+          >
+            <Avatar
+              variant="rounded"
+              sizes="large"
+              src={avatar && null}
+              alt={author}
+              css={css`
+                background-color: ${myBlue};
+                width: 45px;
+                height: 45px;
+              `}
+            >
+              {!avatar && author[0]}
+            </Avatar>
+            <Box>
+              <Typography variant="subtitle2">
+                <Moment fromNow>{date}</Moment>
+              </Typography>
+              <Typography
+                variant="h5"
+                component="p"
+                css={css`
+                  display: flex;
+                  align-items: center;
+                  justify-content: space-between;
+                  gap: 1rem;
+                `}
+              >
+                @{author}
+              </Typography>
+            </Box>
+          </Box>
+          <Box>
+            <MoreHorizIcon
+              css={css`
+                cursor: pointer;
+                align-self: flex-start;
+              `}
+            />
+          </Box>
+        </Box>
+        <Typography variant="h4" component="h1">
           {description}
         </Typography>
       </Box>
-        {photo && <img src={photo} alt={description} />}
+      {photo && <img src={photo} alt={description} />}
     </Paper>
   )
 }
+
+export default Post
