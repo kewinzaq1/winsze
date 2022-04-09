51266ccb6ccd0415f2bc34d32ff76f477a565622
Kewin Szlezingier <77052270+kewinzaq1@users.noreply.github.com>
2022-04-09 16:29:53 +0200

    FIX: useLocalStorageState()

--- src/Utils/hooks.js
+++ src/Utils/hooks.js
@@ -1,28 +1,27 @@
 import {useEffect, useState} from 'react'
 
 function useOverflowHidden() {
-  return useEffect(() => {
-    document.querySelector('body').style.overflow = 'hidden'
-    return () => {
-      document.querySelector('body').style.overflow = 'visible'
-    }
-  }, [])
+    return useEffect(() => {
+        document.querySelector('body').style.overflow = 'hidden'
+        return () => {
+            document.querySelector('body').style.overflow = 'visible'
+        }
+    }, [])
 }
 
-function useLocalStorageState(name, initValue = '') {
-  const getValueFromLocalStorage = window.localStorage.getItem(name)
+function useLocalStorageState(name, initValue) {
+    const getValueFromLocalStorage = window.localStorage.getItem(name)
 
-  const [state, setState] = useState(
-    name !== 'undefined'
-      ? JSON.parse(getValueFromLocalStorage)
-      : initValue ?? null,
-  )
 
-  useEffect(() => {
-    window.localStorage.setItem(name, JSON.stringify(state))
-  }, [name, state])
+    const [state, setState] = useState(
+        getValueFromLocalStorage ?? initValue
+    )
 
-  return [state, setState]
+    useEffect(() => {
+        window.localStorage.setItem(name, JSON.stringify(state))
+    }, [name, state])
+
+    return [state, setState]
 }
 
 export {useOverflowHidden, useLocalStorageState}
