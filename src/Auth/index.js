325ace8f5ac2e89b7a12e63885b2d30146f635dd
Kewin Szlezingier <77052270+kewinzaq1@users.noreply.github.com>
2022-04-10 12:12:47 +0200

    REPLACE: api key location

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -8,13 +8,13 @@ import {useEffect} from 'react'
 import {useLocalStorageState} from '../Utils/hooks'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDgPawN4QvQwjjIp5x_D_ktEY1hxzwLsKU',
-    authDomain: 'winsze.firebaseapp.com',
-    databaseURL: 'https://winsze-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'winsze',
-    storageBucket: 'winsze.appspot.com',
-    messagingSenderId: '564098727958',
-    appId: '1:564098727958:web:3951cf9519225ec0043537',
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  databaseURL: 'https://winsze-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'winsze',
+  storageBucket: 'winsze.appspot.com',
+  messagingSenderId: '564098727958',
+  appId: '1:564098727958:web:3951cf9519225ec0043537',
 }
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
@@ -23,55 +23,54 @@ export const AuthContext = createContext()
 export const db = getFirestore(app)
 
 export const AuthProvider = ({initUser, children} = {}) => {
-    const [user, setUser] = useLocalStorageState('user', initUser)
-    const [status, setStatus] = useState(user ? 'authenticated' : 'login')
-    const [isLoading, setIsLoading] = useState(false)
-    const isLogin = status === 'login'
-    const isRegister = status === 'register'
-    const isAuthenticated = status === 'authenticated'
-    const setLogin = () => setStatus('login')
-    const setRegister = () => setStatus('register')
-    const logout = async () => signOut(auth)
+  const [user, setUser] = useLocalStorageState('user', initUser)
+  const [status, setStatus] = useState(user ? 'authenticated' : 'login')
+  const [isLoading, setIsLoading] = useState(false)
+  const isLogin = status === 'login'
+  const isRegister = status === 'register'
+  const isAuthenticated = status === 'authenticated'
+  const setLogin = () => setStatus('login')
+  const setRegister = () => setStatus('register')
+  const logout = async () => signOut(auth)
 
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        setUser(user)
+        setStatus('authenticated')
+      } else {
+        setUser('')
+        setStatus('login')
+      }
+    })
+  }, [setUser])
 
-    useEffect(() => {
-        onAuthStateChanged(auth, user => {
-            if (user) {
-                setUser(user)
-                setStatus('authenticated')
-            } else {
-                setUser('')
-                setStatus('login')
-            }
-        })
-    }, [setUser])
-
-    return (
-        <AuthContext.Provider
-            value={{
-                user,
-                setUser,
-                status,
-                setStatus,
-                isLogin,
-                isRegister,
-                isAuthenticated,
-                setLogin,
-                setRegister,
-                logout,
-                isLoading,
-                setIsLoading,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        status,
+        setStatus,
+        isLogin,
+        isRegister,
+        isAuthenticated,
+        setLogin,
+        setRegister,
+        logout,
+        isLoading,
+        setIsLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export const useAuth = () => {
-    const context = useContext(AuthContext)
-    if (!context) {
-        throw new Error(`useAuth used only within AuthProvider`)
-    }
-    return context
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error(`useAuth used only within AuthProvider`)
+  }
+  return context
 }
