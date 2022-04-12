f38a6f2ed0b4b5fffcc1f6afc81fa6820dd06f04
Kewin Szlezingier <77052270+kewinzaq1@users.noreply.github.com>
2022-04-12 18:04:09 +0200

    ADD: useStatus() hook

--- src/Utils/hooks.js
+++ src/Utils/hooks.js
@@ -1,25 +1,33 @@
 import {useEffect, useState} from 'react'
 
-function useOverflowHidden() {
-    return useEffect(() => {
-        document.querySelector('body').style.overflow = 'hidden'
-        return () => {
-            document.querySelector('body').style.overflow = 'visible'
-        }
-    }, [])
+const useOverflowHidden = () => {
+  return useEffect(() => {
+    document.querySelector('body').style.overflow = 'hidden'
+    return () => {
+      document.querySelector('body').style.overflow = 'visible'
+    }
+  }, [])
 }
 
-function useLocalStorageState(name, initValue = '') {
-    const getValueFromLocalStorage = window.localStorage.getItem(name)
-    const [state, setState] = useState(
-        getValueFromLocalStorage ?? initValue
-    )
+const useLocalStorageState = (name, initValue = '') => {
+  const getValueFromLocalStorage = window.localStorage.getItem(name)
+  const [state, setState] = useState(getValueFromLocalStorage ?? initValue)
 
-    useEffect(() => {
-        window.localStorage.setItem(name, JSON.stringify(state))
-    }, [name, state])
+  useEffect(() => {
+    window.localStorage.setItem(name, JSON.stringify(state))
+  }, [name, state])
 
-    return [state, setState]
+  return [state, setState]
 }
 
-export {useOverflowHidden, useLocalStorageState}
+const useStatus = () => {
+  const [status, setStatus] = useState(null)
+
+  const isSuccess = status === null
+  const isError = status === 'error'
+  const isLoading = status === 'loading'
+
+  return {status, setStatus, isSuccess, isError, isLoading}
+}
+
+export {useOverflowHidden, useLocalStorageState, useStatus}
